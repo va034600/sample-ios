6a9126e9bb8b865716c663d5557b94458df69b00
va034600 <va034600@gmail.com>
2015-02-18 10:52:53 +0900

    initial commit

---
+++ NewWindow/NewWindow/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  NewWindow
+//
+//  Created by eu on 2015/02/18.
+//  Copyright (c) 2015年 eu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)clickByAlertButton:(id)sender {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"アラートタイトル"
+message:@"アラートメッセージ"
+delegate:self
+cancelButtonTitle:nil
+otherButtonTitles:@"OK", nil];
+    [alertView show];
+}
+@end
