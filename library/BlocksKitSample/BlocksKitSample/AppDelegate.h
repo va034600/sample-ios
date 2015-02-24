b1a2d2ad66d86d34ec719b20b15f985f0214fbfd
va034600 <va034600@gmail.com>
2015-02-24 16:00:30 +0900

    initial commit

---
+++ library/BlocksKitSample/BlocksKitSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocksKitSample
+//
+//  Created by eu on 2015/02/24.
+//  Copyright (c) 2015年 eu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
