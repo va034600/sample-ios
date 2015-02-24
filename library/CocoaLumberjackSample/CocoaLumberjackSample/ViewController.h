73a51d0f693e85e5c3a3d7c90c54f9c418819d84
va034600 <va034600@gmail.com>
2015-02-24 17:12:58 +0900

    initial commit

---
+++ library/CocoaLumberjackSample/CocoaLumberjackSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaLumberjackSample
+//
+//  Created by eu on 2015/02/24.
+//  Copyright (c) 2015年 va034600. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
