a50e082fce41f44552c336cb43f02d93579781e3
va034600 <va034600@gmail.com>
2015-02-28 08:17:42 +0900

    initial commit

---
+++ basic/ScrollViewSample/ScrollViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewSample
+//
+//  Created by eu on 2015/02/28.
+//  Copyright (c) 2015年 va034600. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UIScrollViewDelegate>
+
+
+@end
+
---
+++ basic/ScrollViewSample/ScrollViewSample/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  ScrollViewSample
+//
+//  Created by eu on 2015/02/28.
+//  Copyright (c) 2015年 va034600. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.bouncesZoom = YES;
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    self.scrollView.showsVerticalScrollIndicator = NO;
+    self.scrollView.delegate = self;
+    self.scrollView.minimumZoomScale = 1.0;
+    self.scrollView.maximumZoomScale = 4.0;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// ズームイベントの実装
+- (UIView *) viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+@end
