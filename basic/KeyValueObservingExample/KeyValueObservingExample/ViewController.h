9cfdc804e2fe9945da143e61f51bf5a935e244e3
va034600 <va034600@gmail.com>
2015-03-09 15:31:12 +0900

    key value observing

--- basic/KeyValueObservingExample/KeyValueObservingExample/ViewController.h
+++ basic/KeyValueObservingExample/KeyValueObservingExample/ViewController.h
@@ -10,7 +10,7 @@
 
 @interface ViewController : UIViewController
 
-@property(nonatomic,assign) NSUInteger value;
+@property(nonatomic,assign) NSUInteger value2;
 
 @end
 
--- basic/KeyValueObservingExample/KeyValueObservingExample/ViewController.m
+++ basic/KeyValueObservingExample/KeyValueObservingExample/ViewController.m
@@ -16,7 +16,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self addObserver:self forKeyPath:@"value" options:0 context:NULL];
+    [self addObserver:self forKeyPath:@"value2" options:0 context:NULL];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,11 +25,11 @@
 }
 
 - (IBAction)incrementButtonPressed:(id)sender {
-    self.value++;
+    self.value2++;
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
-    NSLog(@"%d", self.value);
+    NSLog(@"%d", self.value2);
 }
 @end
