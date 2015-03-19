8946b4283c8ece7dec7c363c3d97b60c7429c96f
va034600 <va034600@gmail.com>
2015-03-19 10:13:26 +0900

    alert ios8対応

--- basic/AlertView/AlertView/ViewController.m
+++ basic/AlertView/AlertView/ViewController.m
@@ -14,23 +14,34 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)showAlert:(UIButton *)sender {
+- (IBAction)showAlert:(UIButton *)sender
+{
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"アラートタイトル"
-                                                        message:@"アラートメッセージ"
-                                                       delegate:self
-                                              cancelButtonTitle:nil
-                                              otherButtonTitles:@"OK", nil];
+                              message:@"アラートメッセージ"
+                              delegate:nil
+                              cancelButtonTitle:nil
+                              otherButtonTitles:@"OK", nil];
     [alertView show];
+}
+
+- (IBAction)showAlert2:(id)sender
+{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"TITLE" message:@"message" preferredStyle:UIAlertControllerStyleAlert];
+
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 @end
