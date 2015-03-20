551c1d4464de497c9278126065a629c95133ade2
va034600 <va034600@gmail.com>
2015-03-20 17:59:42 +0900

    TableView Sample

---
+++ basic/TableViewSample/TableViewSample/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  TableViewSample
+//
+//  Created by eu on 2015/03/20.
+//  Copyright (c) 2015年 va034600. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController (){
+    NSArray *array;
+}
+@property (weak, nonatomic) IBOutlet UITableView *abcTableView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.abcTableView.dataSource = self;
+
+    array = [[NSArray alloc] initWithObjects:@"aaa", @"bbb", @"ccc", nil];
+    [self.abcTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+    [self.abcTableView reloadData];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [array count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = [array objectAtIndex: indexPath.row];
+    
+    return cell;
+}
+
+@end
